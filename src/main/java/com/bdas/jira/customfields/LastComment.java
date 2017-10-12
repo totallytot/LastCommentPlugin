73669a40319c7334c5271ae9290d6591459b7404
AbstractElement <fcukthepop@gmail.com>
2017-10-12 17:45:43 +0300

    Add files via upload

---
+++ src/main/java/com/bdas/jira/customfields/LastComment.java
@@ -0,0 +1,67 @@
+package com.bdas.jira.customfields;
+
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.issue.RendererManager;
+import com.atlassian.jira.issue.comments.Comment;
+import com.atlassian.jira.issue.comments.CommentManager;
+import com.atlassian.jira.issue.customfields.impl.GenericTextCFType;
+import com.atlassian.jira.issue.fields.TextFieldCharacterLengthValidator;
+import com.atlassian.jira.issue.fields.renderer.IssueRenderContext;
+import com.atlassian.jira.issue.fields.renderer.JiraRendererPlugin;
+import com.atlassian.jira.security.JiraAuthenticationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.atlassian.jira.issue.customfields.impl.TextCFType;
+import com.atlassian.jira.issue.customfields.manager.GenericConfigManager;
+import com.atlassian.jira.issue.customfields.persistence.CustomFieldValuePersister;
+import com.atlassian.jira.issue.customfields.impl.FieldValidationException;
+import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.issue.fields.CustomField;
+import com.atlassian.jira.issue.fields.config.FieldConfig;
+import com.atlassian.jira.issue.fields.layout.field.FieldLayoutItem;
+import java.util.List;
+import java.util.Map;
+
+public class LastComment extends GenericTextCFType {
+    private static final Logger log = LoggerFactory.getLogger(LastComment.class);
+
+    public LastComment(CustomFieldValuePersister customFieldValuePersister, GenericConfigManager genericConfigManager, TextFieldCharacterLengthValidator textFieldCharacterLengthValidator, JiraAuthenticationContext jiraAuthenticationContext) {
+        super(customFieldValuePersister, genericConfigManager, textFieldCharacterLengthValidator, jiraAuthenticationContext);
+    }
+
+    @Override
+    public boolean isRenderable() {
+        return true;
+    }
+
+    @Override
+    public Map<String, Object> getVelocityParameters(final Issue issue,
+                                                     final CustomField field,
+                                                     final FieldLayoutItem fieldLayoutItem) {
+        final Map<String, Object> map = super.getVelocityParameters(issue, field, fieldLayoutItem);
+
+        // This method is also called to get the default value, in
+        // which case issue is null so we can't use it to add currencyLocale
+        if (issue == null) {
+            return map;
+        }
+
+        FieldConfig fieldConfig = field.getRelevantConfig(issue);
+         //add what you need to the map here
+
+        CommentManager commentManager = ComponentAccessor.getCommentManager();
+        Comment comment = commentManager.getLastComment(issue);
+        String lastComment = "";
+        if (comment != null) {
+            lastComment = comment.getBody();
+            RendererManager rendererManager = ComponentAccessor.getRendererManager();
+            JiraRendererPlugin wikiRenderer = rendererManager.getRendererForType("atlassian-wiki-renderer");
+            IssueRenderContext renderContext = new IssueRenderContext(issue);
+            lastComment = wikiRenderer.render(lastComment, renderContext);
+        }
+
+        map.put("lastComment", lastComment);
+
+        return map;
+    }
+}
---
+++ src/test/java/ut/com/bdas/jira/customfields/LastCommentTest.java
@@ -0,0 +1,34 @@
+package ut.com.bdas.jira.customfields;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import com.bdas.jira.customfields.LastComment;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @since 3.5
+ */
+public class LastCommentTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test(expected=Exception.class)
+    public void testSomething() throws Exception {
+
+        //LastComment testClass = new LastComment();
+
+        throw new Exception("LastComment has no tests!");
+
+    }
+
+}
